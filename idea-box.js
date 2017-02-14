0f8483f0d20dbe3e59b87495203f66d2a6c69c05
Jennifer Peavler <jenn.peavler@gmail.com>
2017-02-14 14:01:27 -0700

    Make title and idea content editable.  Add save button functionality.  Add upvote/downvote functionality

--- idea-box.js
+++ idea-box.js
@@ -5,82 +5,67 @@ $('#save-button').on('click', function() {
   $('#display-side').prepend(
     `<div class='idea-card'>
       <div id='line-1'>
-        <h2>${$ideaTitle}</h2>
+        <h2 contenteditable='true'>${$ideaTitle}</h2>
         <button id='delete-button'>Delete</button>
       </div>
-      <p id='line-2'>${$ideaContent}</p>
+      <p id='line-2' contenteditable='true'>${$ideaContent}</p>
       <div id='line-3'>
-        <button id='upvote-button'>up</button>
+        <button id='upvote-button'>
+          <img src="images/upvote.svg" alt="">
+        </button>
         <button id='downvote-button'>down</button>
-        <p>quality:  swill</p>
+        <p id='quality-line'>quality:  <span>swill</span></p>
       </div>
      </div>`);
 
   $('#idea-title').val('');
   $('#idea-content').val('');
 
+// <button><img src="images/add.gif" alt="add"
+// width="10" height="10" /> Add</button>
+
+});
+
+$('#display-side').on('click', '#upvote-button', function () {
+  var $qualityText = $(this).siblings('#quality-line').children();
+  if ($qualityText.text() === 'swill') {
+    $qualityText.text('plausible');
+  } else if ($qualityText.text() === 'plausible') {
+    $qualityText.text('genius');
+  }
+});
+
+$('#display-side').on('click', '#downvote-button', function () {
+  var $qualityText = $(this).siblings('#quality-line').children();
+  if ($qualityText.text() === 'genius') {
+    $qualityText.text('plausible');
+  } else if ($qualityText.text() === 'plausible') {
+    $qualityText.text('swill');
+  }
 });
 
 
 //global variables
-var $ideaTitle = $('idea-title');
-var $ideaContent = $('idea-title');
-
-//clear fields function add to end of save button
-function clearField () {
-  $ideaTitle.val("");
-  $ideaContent.val("");
-}
-
-//NOTE: Found code pen on making content editable by user. .onBlur() function to commit changes????
-// <div contenteditable="true">
-//   This text can be edited by the user.
-// </div>
-
-//save button clear fields
-$('save-button').on('click', function () {
-  clearField();
-})
-
-// UPVOTE BTN quality span needs ID for sibling ref
-$('#display-side').on('click', '#upvote-button', function () {
-  var $qualityText = $(this).siblings($('#quality').text())
-
-  if ($qualityText === 'swill') {
-    $qualityText.text('plausible')
-  } else if ($qualityText === 'plausible') {
-    $qualityText.text('genius')
-  }
-})
+// var $ideaTitle = $('idea-title');
+// var $ideaContent = $('idea-title');
 
 
-// DWNVOTE BTN quality span needs ID for sibling ref
-$('#display-side').on('click', '#downvote-button', function() {
-  var $qualityText = $(this).siblings($('#quality').text());
-
-  if ($qualityText === 'genius') {
-    $qualityText.text('plausible')
-  } else if ($qualityText === 'plausible') {
-    $qualityText.text('genius')
-  })
-})
-
-//Deleting an idea card
-$('#display-side').on('click', '#delete-button', function() {
-  $(this).parent('.idea-card').remove();
-})
-
-
-//Search Bar ideas
-
-//on keyup run filtering function
-$('#search').on('keyup',function {
-
-//compare search value to card title
-
-//compare search value to card idea
-
-//remove cards that are not matching
-
-//when deleting cards should repopulate
-})
+// //Deleting an idea card
+// $('#display-side').on('click', '#delete-button', function() {
+//   $(this).parent('.idea-card').remove();
+// })
+//
+//
+// //Search Bar ideas
+//
+// //on keyup run filtering function
+// $('#search').on('keyup',function {
+//
+// //compare search value to card title
+//
+// //compare search value to card idea
+//
+// //remove cards that are not matching
+//
+// //when deleting cards should repopulate
+// })
