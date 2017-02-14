a15443b30f60f258cf5f562639545e0c5c855acb
Jennifer Peavler <jenn.peavler@gmail.com>
2017-02-14 10:39:37 -0700

    Add css to idea-card. Add js to prepend idea-card

--- idea-box.js
+++ idea-box.js
@@ -1,6 +1,22 @@
-$('#save-button').on('click' function() {
+$('#save-button').on('click', function() {
   var $ideaTitle = $('#idea-title').val();
   var $ideaContent = $('#idea-content').val();
 
-  $('#display-side').prepend("<div class='idea-card'><h2>" + $ideaTitle + "</h2><button id='delete-button'>x</button><p>" + $ideaContent + "</p><button id='upvote-button'>x</button><buttonid='downvote-button'>x</button><p>quality: swill</p></div>");
+  $('#display-side').prepend(
+    `<div class='idea-card'>
+      <div id='line-1'>
+        <h2>${$ideaTitle}</h2>
+        <button id='delete-button'>Delete</button>
+      </div>
+      <p id='line-2'>${$ideaContent}</p>
+      <div id='line-3'>
+        <button id='upvote-button'>up</button>
+        <button id='downvote-button'>down</button>
+        <p>quality:  swill</p>
+      </div>
+     </div>`);
+
+  $('#idea-title').val('');
+  $('#idea-content').val('');
+
 });
