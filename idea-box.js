b0bb25f7e8d604caa47448dadc4c8f52c218ddac
Jennifer Peavler <jenn.peavler@gmail.com>
2017-02-13 17:40:32 -0700

    add js

--- idea-box.js
+++ idea-box.js
@@ -1,7 +1,7 @@
 $('#save-button').on('click' function() {
-  var $ideaTitle = $('#idea-title');
-  var $ideaContent = $('#idea-content');
+  var $ideaTitle = $('#idea-title').val();
+  var $ideaContent = $('#idea-content').val();
 
   $('#display-side').prepend(
-    "<div class='idea-card'><h2>" + $ideaTitle.val() + "</h2><button id='delete-button'>x</button><p>" + $ideaContent.val() + "</p><button id='upvote-button'>x</button><buttonid='downvote-button'>x</button><p>quality: swill</p></div>"
+    "<div class='idea-card'><h2>" + $ideaTitle + "</h2><button id='delete-button'>x</button><p>" + $ideaContent + "</p><button id='upvote-button'>x</button><buttonid='downvote-button'>x</button><p>quality: swill</p></div>"
 });
