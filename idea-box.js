0a575c89791bd90f0134c85b46d03047fa8ca307
Jennifer Peavler <jenn.peavler@gmail.com>
2017-02-13 21:44:13 -0700

    Correct syntax

--- idea-box.js
+++ idea-box.js
@@ -2,6 +2,5 @@ $('#save-button').on('click' function() {
   var $ideaTitle = $('#idea-title').val();
   var $ideaContent = $('#idea-content').val();
 
-  $('#display-side').prepend(
-    "<div class='idea-card'><h2>" + $ideaTitle + "</h2><button id='delete-button'>x</button><p>" + $ideaContent + "</p><button id='upvote-button'>x</button><buttonid='downvote-button'>x</button><p>quality: swill</p></div>"
+  $('#display-side').prepend("<div class='idea-card'><h2>" + $ideaTitle + "</h2><button id='delete-button'>x</button><p>" + $ideaContent + "</p><button id='upvote-button'>x</button><buttonid='downvote-button'>x</button><p>quality: swill</p></div>");
 });
