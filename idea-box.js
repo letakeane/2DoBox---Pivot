4279ab9dd34b0454d458ae37718b7036d3a9d88e
Jennifer Peavler <jenn.peavler@gmail.com>
2017-02-14 23:02:41 -0700

    Add persistence of idea-cards on page load.  Delete button functionality deletes idea-card from local storage.

--- idea-box.js
+++ idea-box.js
@@ -1,27 +1,61 @@
-  function Idea(id, title, body) {
+function Idea(id, title, body) {
   this.id = id;
   this.title = title;
   this.body = body;
 }
 
-  $('#save-button').on('click', function() {
-  var $ideaTitle = $('#idea-title').val();
-  var $ideaContent = $('#idea-content').val();
-  var id = event.timeStamp;
-  var newIdea = new Idea(id, $ideaTitle, $ideaContent);
-  console.log(newIdea);
-
-  function stringifyObj (newIdea) {
-    var stringifiedIdea = JSON.stringify(newIdea);
-    console.log(stringifiedIdea);
-    localStorage.setItem(id, stringifiedIdea);
-  }
-  stringifyObj(newIdea);
-
-
+$(document).ready(function () {
+console.log("Document Loaded");
+for(var i=0;i<localStorage.length;i++)
+{
+  var obj = localStorage.getItem(localStorage.key(i));
+  var parsedobj = JSON.parse(obj);
+  var $ideaTitle = parsedobj.title;
+  var $ideaContent = parsedobj.body;
+  var $id = parsedobj.id;
 
   $('#display-side').prepend(
-    `<div class='idea-card'>
+    `<div class='idea-card' id=${$id}>
+      <div id='line-1'>
+        <h2 contenteditable='true'>${$ideaTitle}</h2>
+        <button id='delete-button'>Delete</button>
+      </div>
+      <p id='line-2' contenteditable='true'>${$ideaContent}</p>
+      <div id='line-3'>
+        <button id='upvote-button'>
+          <img src="images/upvote.svg" width='20' height='20' alt="">
+        </button>
+        <button id='downvote-button'>down</button>
+        <p id='quality-line'>quality:  <span id="qual">swill</span></p>
+      </div>
+     </div>`);
+}
+});
+
+
+
+$('#save-button').on('click', function() {
+  var $ideaTitle = $('#idea-title').val();
+  var $ideaContent = $('#idea-content').val();
+  var $id = $.now();
+  var newIdea = new Idea($id, $ideaTitle, $ideaContent);
+  console.log(newIdea);
+
+  // function stringifyObj (newIdea) {
+
+    var stringifiedIdea = JSON.stringify(newIdea);
+    console.log(stringifiedIdea);
+    localStorage.setItem($id, stringifiedIdea);
+    // var retObj = localStorage.getItem($id);
+    // console.log(retObj);
+    // var parsedObj = JSON.parse(retObj);
+    // console.log(parsedObj);
+  // }
+
+  // stringifyObj(newIdea);
+
+  $('#display-side').prepend(
+    `<div class='idea-card' id=${$id}>
       <div id='line-1'>
         <h2 contenteditable='true'>${$ideaTitle}</h2>
         <button id='delete-button'>Delete</button>
@@ -36,16 +70,11 @@
       </div>
      </div>`);
 
-    //  var numCards = $(".idea-card");
-    //  var stringed = JSON.stringify(numCards);
-    //  localStorage.setItem("huh", stringed);
-    //  console.log(numCards);
-    //  var retObj = localStorage.getItem("huh");
-    //  var parsedObj = JSON.parse(retObj);
-    //  console.log(parsedObj);
-
   $('#idea-title').val('');
   $('#idea-content').val('');
+
+
+
 });
 
 
@@ -71,6 +100,8 @@ $('#display-side').on('click', '#downvote-button', function () {
 $('#display-side').on('click', '#delete-button', function() {
   var $whatIsDeleted = $(this).closest('.idea-card');
   $whatIsDeleted.remove();
+  var idValue = $whatIsDeleted.attr('id');
+  localStorage.removeItem(idValue);
 });
 
 
