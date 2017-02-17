d2f09437e7fc1cea6f559a72a5469faa264cc533
Zane Russell <zaneroosa@gmail.com>
2017-02-17 14:47:30 -0700

    add shared testing file selenium-tests

---
+++ selenium-tests.js
@@ -0,0 +1,17 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver_fx = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+var driver_chr = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+var driver_saf = new webdriver.Builder()
+    .forBrowser('safari')
+    .build();
+
+    
