663f41168bd93467d6340a87b7d20ce1ca034b8f
Zane Russell <zaneroosa@gmail.com>
2017-02-20 16:49:06 -0700

    tests for upvote, downvote, and delete functionality work

--- selenium-tests.js
+++ selenium-tests.js
@@ -19,7 +19,7 @@ Test(driver_chr);
 // Test(driver_saf);
 
 function Test(driver) {
-  driver.get('file:///Users/leta/Turing/2DoBox---Pivot/idea-box.html');
+  driver.get('file:///Users/zanerussell/turing/todobox/idea-box/idea-box.html');
   driver.getTitle().then(function(title) {
     if (title === 'idea-box') {
       console.log('Page load: test passed');
@@ -65,4 +65,65 @@ function Test(driver) {
       }
     });
   });
+
+  var qualityTest = driver.findElement(By.id('qual')).getText();
+  var upvote = driver.findElement(By.id('upvote-button'));
+
+  driver.sleep(1000).then(function(){
+    upvote.click();
+  });
+
+  driver.navigate().refresh();
+  driver.sleep(4000).then(function() {
+    driver.findElement(By.id('qual')).getText().then(function(quality){
+    if( quality === 'plausible'){
+      console.log('Upvote button works')
+    } else {
+      console.log('Upvote button fails')
+    }
+    });
+  });
+
+  var downvote = driver.findElement(By.id('downvote-button'));
+
+  driver.sleep(1000).then(function(){
+    downvote.click();
+  });
+
+  driver.navigate().refresh();
+  driver.sleep(4000).then(function() {
+    driver.findElement(By.id('qual')).getText().then(function(quality){
+    if( quality === 'swill'){
+      console.log('Downvote button works')
+    } else {
+      console.log('Downvote button fails')
+    }
+    });
+  });
+
+  driver.findElement(By.id('idea-title')).sendKeys('Idea title 2');
+  driver.findElement(By.id('idea-content')).sendKeys('Idea content 2');
+
+  driver.sleep(2000).then(function () {
+    driver.findElement(By.id('save-button')).getAttribute(disabled);
+    console.log('Save button disabled: ' + disabled);
+  });
+
+  driver.findElement(By.id('save-button')).click();
+
+  driver.sleep(1000).then(function() {
+    driver.findElement(By.id('delete-button')).then(function(deleteButton){
+      console.log(deleteButton.length)
+      deleteButton.click();
+    });
+  });
+
+  driver.findElement(By.className('titleEdit')).getText().then(function(text) {
+    if (text === 'Idea title 1') {
+      console.log('Delete button test passed');
+    } else {
+      console.log('Delete button test failed');
+    }
+  });
+
 };
